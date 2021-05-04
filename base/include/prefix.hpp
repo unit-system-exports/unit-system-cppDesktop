d2449c317fe6bc6ed020230ef664e722dfcfb16f
noasakurajin <noasakurajin@web.de>
2021-05-04 19:10:51 +0200

    more ci corrections

--- base/include/prefix.hpp
+++ base/include/prefix.hpp
@@ -13,16 +13,16 @@ namespace sakurajin{
             const double milli = 0.001;
             const double centi = 0.01;
             const double deci  = 0.1;
-            const double deca  = 10;
-            const double hekto = 100;
-            const double kilo  = 1000;
-            const double mega  = 1000000;
-            const double giga  = 1000000000;
-            const double tera  = 1000000000000;
-            const double peta  = 1000000000000000;
-            const double exa   = 1000000000000000000;
-            const double zetta = 1000000000000000000000;
-            const double yotta = 1000000000000000000000000;
+            const double deca  = 10.0;
+            const double hekto = 100.0;
+            const double kilo  = 1000.0;
+            const double mega  = 1000000.0;
+            const double giga  = 1000000000.0;
+            const double tera  = 1000000000000.0;
+            const double peta  = 1000000000000000.0;
+            const double exa   = 1000000000000000000.0;
+            const double zetta = 1000000000000000000000.0;
+            const double yotta = 1000000000000000000000000.0;
         }
     }
 }
