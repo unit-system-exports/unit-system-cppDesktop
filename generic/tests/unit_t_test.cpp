d2eb7a8f994be05143ad5c6055ab131f89efb6c7
noasakurajin <dev@noasakurajin.de>
2022-08-17 11:45:37 +0200

    updating generic unit tests to gtest

--- generic/tests/unit_t_test.cpp
+++ generic/tests/unit_t_test.cpp
@@ -1,15 +1,22 @@
-#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 
 #include "unit_t.hpp"
-#include "test_helper.hpp"
 #include "prefix.hpp"
 
 #include <iostream>
 
+//a simple way to check if to bools are equal
+#include <cmath>
+#include <algorithm>
+bool isDoubleEqual(long double v1, long double v2){
+    auto delta = v1-v2;
+    return std::abs(delta) < 0.000001 * std::max(std::abs(v1), std::abs(v1));
+}
+
 using namespace sakurajin::unit_system;
 using namespace sakurajin::unit_system::prefix;
 
-TEST_CASE( "Checking if casting works", "[vector]" ) {
+TEST( unit_t_tests, cast_tests ) {
     
     const unit_t<0> v1{1000,1};
     const unit_t<0> v2{1,1000};
@@ -19,18 +26,18 @@ TEST_CASE( "Checking if casting works", "[vector]" ) {
     const unit_t<0> v6{-1,kilo};
     
     
-    REQUIRE(v1 == v2);
-    REQUIRE(v1 == v3);
-    REQUIRE(v2 == v3);
-    REQUIRE(v2 == v5);
+    EXPECT_TRUE(v1 == v2);
+    EXPECT_TRUE(v1 == v3);
+    EXPECT_TRUE(v2 == v3);
+    EXPECT_TRUE(v2 == v5);
     
-    REQUIRE(v1 != v4);
+    EXPECT_TRUE(v1 != v4);
     
-    REQUIRE(abs(v5) == abs(v6));
+    EXPECT_TRUE(abs(v5) == abs(v6));
     
 };
 
-TEST_CASE( "Checking if comparison works", "[vector]" ) {
+TEST( unit_t_tests, compare_tests) {
     
     const unit_t<0> v1{1000,1};
     const unit_t<0> v2{1,1000};
@@ -38,23 +45,23 @@ TEST_CASE( "Checking if comparison works", "[vector]" ) {
     const unit_t<0> v4{1,1};
     const unit_t<0> v5{1,kilo};
     
-    REQUIRE(v4 < v5);
-    REQUIRE(v5 > v4);
-    REQUIRE(v4 <= v5);
-    REQUIRE(v5 >= v4);
+    EXPECT_TRUE(v4 < v5);
+    EXPECT_TRUE(v5 > v4);
+    EXPECT_TRUE(v4 <= v5);
+    EXPECT_TRUE(v5 >= v4);
     
-    REQUIRE(v1 <= v1);
-    REQUIRE(v1 >= v1);
+    EXPECT_TRUE(v1 <= v1);
+    EXPECT_TRUE(v1 >= v1);
     
     #if __cplusplus >= 202002L
-    REQUIRE( (v4 <=> v5) < 0)
-    REQUIRE( (v5 <=> v4) > 0)
-    REQUIRE( (v1 <=> v1) == 0)
+    EXPECT_TRUE( (v4 <=> v5) < 0)
+    EXPECT_TRUE( (v5 <=> v4) > 0)
+    EXPECT_TRUE( (v1 <=> v1) == 0)
     #endif
     
 };
 
-TEST_CASE( "Checking if operators works", "[vector]" ) {
+TEST( unit_t_tests, operator_tests ) {
     
     const unit_t<0> v1{1000,1};
     const unit_t<0> v2{1,1000};
@@ -63,21 +70,26 @@ TEST_CASE( "Checking if operators works", "[vector]" ) {
     const unit_t<0> v5{999,1};
     
     auto _v4 = v4;
-    REQUIRE(v2 != _v4);
+    EXPECT_TRUE(v2 != _v4);
     _v4 = v2;
-    REQUIRE(v2 == _v4);
+    EXPECT_TRUE(v2 == _v4);
     
     auto v6 = v1 + v2;
-    REQUIRE(isDoubleEqual(v6.value, 2000.0));
-    REQUIRE(v6 == v1*2.0);
-    REQUIRE(v1 == v6/2.0);
+    EXPECT_TRUE(isDoubleEqual(v6.value, 2000.0));
+    EXPECT_TRUE(v6 == v1*2.0);
+    EXPECT_TRUE(v1 == v6/2.0);
     
     auto v7 = v2 + v1;
-    REQUIRE(isDoubleEqual(v7.value, 2.0));
-    REQUIRE(v6 == v7);
+    EXPECT_TRUE(isDoubleEqual(v7.value, 2.0));
+    EXPECT_TRUE(v6 == v7);
     
     auto v8 = v1-v4;
-    REQUIRE(v5 == v8);
+    EXPECT_TRUE(v5 == v8);
     
 };
+
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
 
