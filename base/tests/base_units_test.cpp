cd2fc9ea3df4a6bc9f5b5693c22c8625d88e7a9b
noasakurajin <dev@noasakurajin.de>
2022-08-17 11:54:07 +0200

    updated base units unit tests to gtest

--- base/tests/base_units_test.cpp
+++ base/tests/base_units_test.cpp
@@ -1,8 +1,15 @@
-#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 
-#include "test_helper.hpp"
 #include "base.hpp"
 #include "prefix.hpp"
+
+//a simple way to check if to bools are equal
+#include <cmath>
+#include <algorithm>
+bool isDoubleEqual(long double v1, long double v2){
+    auto delta = v1-v2;
+    return std::abs(delta) < 0.000001 * std::max(std::abs(v1), std::abs(v1));
+}
 
 #include <iostream>
 
@@ -11,56 +18,60 @@ using namespace sakurajin::unit_system::prefix;
 using namespace sakurajin::unit_system::base::literals;
 using namespace sakurajin::unit_system::base::constants;
 
-TEST_CASE( "Checking if time works", "[vector]" ) {
+TEST( base_units_tests, time_test ) {
     
     const auto t1 = 1_ms;
-    REQUIRE( t1*1000 == 1.0_s);
+    EXPECT_TRUE( t1*1000 == 1.0_s);
     
 };
 
-TEST_CASE( "Checking if length works", "[vector]" ) {
+TEST( base_units_tests, length_test ) {
     
     const auto s1 = 1_mm;
-    REQUIRE( s1*1000 == 1.0_m);
+    EXPECT_TRUE( s1*1000 == 1.0_m);
 
 };
 
-TEST_CASE( "Checking if mass works", "[vector]" ) {
+TEST( base_units_tests, mass_test ) {
     
     const auto m1 = 1_g;
-    REQUIRE( m1*1000 == 1.0_kg);
+    EXPECT_TRUE( m1*1000 == 1.0_kg);
 
 };
 
-TEST_CASE( "Checking if electric current works", "[vector]" ) {
+TEST( base_units_tests, electric_current_test ) {
     
     const auto I1 = 1_A;
-    REQUIRE( I1*1000 == 1.0_kA);
+    EXPECT_TRUE( I1*1000 == 1.0_kA);
 
 };
 
-TEST_CASE( "Checking if luminous intensity works", "[vector]" ) {
+TEST( base_units_tests, lum_intensity_test ) {
     
     const auto J1 = 1_cd;
-    REQUIRE( J1*1000 == 1000.0_cd);
+    EXPECT_TRUE( J1*1000 == 1000.0_cd);
 
 };
 
-TEST_CASE( "Checking if temperature works", "[vector]" ) {
+TEST( base_units_tests, temparature_test ) {
     
     const auto T1 = 20_C;
-    REQUIRE( T1 == 293.15_K);
+    EXPECT_TRUE( T1 == 293.15_K);
 
 };
 
-TEST_CASE( "Checking if amount works", "[vector]" ) {
+TEST( base_units_tests, amount_test ) {
     
     const auto five = 5.0_things;
-    REQUIRE( isDoubleEqual(static_cast<long double>(five), 5.0));
+    EXPECT_TRUE( isDoubleEqual(static_cast<long double>(five), 5.0));
     
     const auto a1 = 2_mol;
-    REQUIRE( isDoubleEqual(static_cast<long double>(a1), 2.0*avogadro_constant) );
+    EXPECT_TRUE( isDoubleEqual(static_cast<long double>(a1), 2.0*avogadro_constant) );
     
 };
 
 
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
